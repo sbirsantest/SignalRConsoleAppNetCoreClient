a94d18e57078cdfe909f7559f3347460495d91ca
sbarsan <sorinbarsan@gmail.com>
2021-09-13 18:20:13 +0300

    Add project files.

---
+++ SignalRConsoleAppNetCoreClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SignalRConsoleAppNetCoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
